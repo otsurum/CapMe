9dbbf89973f60333b49950708dfa7d8fb274e0f4
鶴見駿 <tsurumishun@tsurumishunnoMacBook-Pro-2.local>
2025-01-08 00:20:02 +0900

    タイマーの表示を１時間経過しているかどうかで小数点以下の表示の有無を制御

--- MECap/MECap/Extensions/Double.swift
+++ MECap/MECap/Extensions/Double.swift
@@ -9,8 +9,13 @@ extension Double {
     func formattedTime() -> String {
         let hours = Int(self) / 3600
         let minutes = (Int(self) % 3600) / 60
-        let seconds = Int(self) % 60
+        let seconds = self.truncatingRemainder(dividingBy: 60)
         
-        return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+        
+        if hours == 0 {
+            return String(format: "%02d:%02d.%01d", minutes, Int(seconds), Int(seconds.truncatingRemainder(dividingBy: 1)*10))
+        } else {
+            return String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+        }
     }
 }
